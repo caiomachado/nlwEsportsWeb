d643d9f7ddea16c76272b996b521511db3f9ecc7
Caio Machado <caiomachado1102@gmail.com>
2022-09-19 01:50:25 -0300

    initial commit

---
+++ src/components/CreateAdBanner.tsx
@@ -0,0 +1,20 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
+import { DialogTrigger } from '@radix-ui/react-dialog';
+
+export function CreateAdBanner() {
+  return (
+    <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+      <div className="bg-[#2A2634] py-6 px-8 flex justify-between items-center">
+        <div>
+          <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+          <span className="text-zinc-400">Publique um anúncio para encontrar novos players!</span>
+        </div>
+
+        <DialogTrigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+          <MagnifyingGlassPlus size={24} />
+          Publicar anúncio
+        </DialogTrigger>
+      </div>
+    </div>
+  )
+}
